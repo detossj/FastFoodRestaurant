deecae2cfc23cd51ff8377844fbcb6f7683ac6c1
detossj <jorgerubilarssj@gmail.com>
2025-11-21 20:49:36 -0300

    created ProtectedRoutes.jsx

---
+++ resources/js/pageAuth/ProtectedRoutes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import AuthUser from './AuthUser'
+
+
+const ProtectedRoutes = () => {
+  const { getToken } = AuthUser()
+
+  if(!getToken()) {
+    return <Navigate to={'/'} />
+  }
+  return (
+    <Outlet/>
+  )
+}
+
+export default ProtectedRoutes
