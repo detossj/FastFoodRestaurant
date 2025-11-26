4e640dee6a7628e290b77bc27bb13f15218546de
detossj <jorgerubilarssj@gmail.com>
2025-11-26 13:56:22 -0300

    changed TopBar.jsx

--- resources/js/components/TopBar.jsx
+++ resources/js/components/TopBar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import logo from '../../assets/images/logo.webp';
 
 const TopBar = () => {
   return (
-    <div>TopBar</div>
-  )
-}
+    <div className="py-2" style={{borderBottom: "1px solid #e5e5e5"}}>
+      <div className="container d-flex justify-content-between align-items-center">
 
-export default TopBar
+          <div className="d-flex align-items-center">
+            <img src={logo} alt="Logo" style={{ height: '70px', cursor: 'pointer' }}/>
+          </div>
+
+          <div className="d-flex align-items-center gap-4">
+
+            <div className="topbar-item">
+              <i className="fa-solid fa-pizza-slice me-1" style={{color: 'orange'}}></i> MENÚ
+            </div>
+
+            <div className="topbar-item">
+              <i className="fa-solid fa-receipt me-1" style={{color: 'orange'}}></i> PEDIDOS
+            </div>
+
+            <div className="topbar-item">
+              <i className="fa-solid fa-store me-1" style={{color: 'orange'}}></i> LOCALES
+            </div>
+
+            <div className="topbar-item">
+              <i className="fa-solid fa-user me-1" style={{color: 'orange'}}></i> INGRESAR
+            </div>
+
+          </div>
+
+      </div>
+    </div>
+
+  );
+};
+
+export default TopBar;
