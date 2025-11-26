357118dd2ecd27f8252627bb9cc4293f9f6a4f80
detossj <jorgerubilarssj@gmail.com>
2025-11-26 13:56:40 -0300

    changed Home.jsx design

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -5,32 +5,50 @@ import pizzaDrinkGarlicsticksImg from '../../assets/images/pizza-drink-garlicsti
 
 const Home = () => {
   return (
-    <div className="row justify-content-center mt-4">
-       <div className="col-12 col-md-8 col-lg-8">
-        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                    <img src={burgerImg} class="d-block w-100" style={{ height: '500px', objectFit: 'cover'}} alt="Hamburguesa con papas"/>
-                    </div>
-                    <div class="carousel-item">
-                    <img src={pizzaDrinkImg} class="d-block w-100" style={{ height: '500px', objectFit: 'cover'}} alt="Pizza con bebida"/>
-                    </div>
-                    <div class="carousel-item">
-                    <img src={pizzaDrinkGarlicsticksImg} class="d-block w-100" style={{ height: '500px', objectFit: 'cover'}} alt="Pizza de pepperoni"/>
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-       </div>
+    <div className="container mt-5">
+
+      <div id="carouselExampleAutoplaying"
+           className="carousel slide"
+           data-bs-ride="carousel"
+           data-bs-interval="3000">
+
+        <div className="carousel-inner">
+
+          <div className="carousel-item active">
+            <img src={burgerImg} className="d-block w-100"
+                 style={{ height: '500px', objectFit: 'cover' }}
+                 alt="Hamburguesa con papas" />
+          </div>
+
+          <div className="carousel-item">
+            <img src={pizzaDrinkImg} className="d-block w-100"
+                 style={{ height: '500px', objectFit: 'cover' }}
+                 alt="Pizza con bebida" />
+          </div>
+
+          <div className="carousel-item">
+            <img src={pizzaDrinkGarlicsticksImg} className="d-block w-100"
+                 style={{ height: '500px', objectFit: 'cover' }}
+                 alt="Pizza de pepperoni" />
+          </div>
+
+        </div>
+
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+
+      </div>
+
     </div>
-  )
-}
+  );
+};
+
 
 export default Home
